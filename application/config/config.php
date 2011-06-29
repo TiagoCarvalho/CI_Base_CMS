071a2e6751c4e682c46f1bd2035f78fb97fffd02
mpmont <mpmont@gmail.com>
2011-06-29 16:57:56 +0100

    Criação de Ficheiros de Lingua PT para mensagens de sistema.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/bloo_admin/';
+$config['base_url']	= 'http://localhost/admin_base/';
 
 /*
 |--------------------------------------------------------------------------
